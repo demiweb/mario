d4f1228469835072220999d86c00f5e90bdb699d
yurayarosh <yura12323@gmail.com>
2019-08-07 15:09:17 +0300

    add registration mob bg, some edits

--- src/js/components/setLazy.js
+++ src/js/components/setLazy.js
@@ -1,10 +1,35 @@
 import lozad from 'lozad';
+import { debounce } from 'throttle-debounce';
 
 export default function lazyLoading() {
   const imgs = [].slice.call(document.querySelectorAll('.js-lazy'));
 
   if(!imgs.length) return;
   imgs.forEach(img => {
+    const imageDesc = img.dataset.backgroundImageDesc;
+    const imageMob = img.dataset.backgroundImageMob;
+
+    if (imageDesc && imageMob) {
+      if (window.matchMedia('(min-width: 576px)').matches) {
+        img.setAttribute('data-background-image', imageDesc);
+      } else {
+        img.setAttribute('data-background-image', imageMob);
+      };
+
+      function setSrc() {
+        const style = window.getComputedStyle(img);
+
+        if (window.matchMedia('(min-width: 576px)').matches && style.backgroundImage) {
+          img.style.backgroundImage = `url(${imageDesc})`;
+        } else if(style.backgroundImage) {
+          img.style.backgroundImage = `url(${imageMob})`;
+        };
+      };
+
+      const setSrcDebounced = debounce(66, setSrc);
+      window.addEventListener('resize', setSrcDebounced);
+    };
+
     const observer = lozad(img);
     observer.observe();
   });
